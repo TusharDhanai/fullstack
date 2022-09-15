c86352d830fe4ce4f922e4001d1bb27da00a3c08
TusharDhanai <dhanai.tushar007@gmail.com>
2022-09-15 19:21:06 +0530

    courseinfo step 9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 const Header = ({course}) => {
     return (
         <div>
-            <h1>{course}</h1>
+            <h2>{course}</h2>
         </div>
     )
 }
@@ -36,34 +36,53 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-    const course = {
-        id:1,
-        name: "Half Stack application development",
-        parts: [
+    const courses = [
+        {
+            id:1,
+            name: "Half Stack application development",
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id:1
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id:2
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id:3
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id:4
+                }
+            ]
+        }, 
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
             {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id:1
+                name: 'Routing',
+                exercises: 3,
+                id: 1
             },
             {
-                name: "Using props to pass data",
+                name: 'Middlewares',
                 exercises: 7,
-                id:2
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id:3
-            },
-            {
-                name: "Redux",
-                exercises: 11,
-                id:4
+                id: 2
             }
-        ]
-    }
+            ]
+        }
+    ]
+
     return (
-        <Course course={course} />
+        courses.map(item => <Course key={item.id} course={item} />)
     )
 }
 
