b7caa64773c257acb082fd14e1a518e9686f9a12
TusharDhanai <dhanai.tushar007@gmail.com>
2022-09-16 14:56:38 +0530

    phonebook, step 4.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { 
-            name: 'Arto Hellas',
-            number: 123906453,
-        }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [searchText, setSearchText] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault();
@@ -19,12 +20,14 @@ const App = () => {
             return;
         }
 
-        setPersons(persons.concat({name: newName,number: newNumber}))
+        setPersons(persons.concat({name: newName,number: newNumber,id: persons.length + 1}))
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            search :<input onInput={event => setSearchText(event.target.value)} autoComplete='off' />
+            <h2>Add new contact</h2>
             <form>
                 <div>
                     name: <input onInput={event => setNewName(event.target.value)} autoComplete="off" />
@@ -38,7 +41,12 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map(item => <p key={item.name}>{item.name} {item.number}</p>)}
+                {
+                    persons.map(item => {
+                        if (item.name.toLowerCase().includes(searchText.toLowerCase()))
+                            return <p key={item.id}>{item.name} {item.number}</p>
+                    })
+                }
             </div>
         </div>
     )
